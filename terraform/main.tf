5698a8c0fd56a5af6ec3ad18e619187d7f994998
willcsilva <willcsilva15@gmail.com>
2026-03-19 19:52:46 -0300

    Novo arquivo DESTROY APIGW

--- terraform/main.tf
+++ terraform/main.tf
@@ -136,10 +136,10 @@ resource "aws_apigatewayv2_api" "http_api" {
 }
 
 resource "aws_apigatewayv2_integration" "lambda_integration" {
-  api_id                 = aws_apigatewayv2_api.http_api.id
-  integration_type       = "AWS_PROXY"
-  integration_method     = "POST"
-  integration_uri        = aws_lambda_function.auth.invoke_arn
+  api_id           = aws_apigatewayv2_api.http_api.id
+  integration_type = "AWS_PROXY"
+  integration_method = "POST"
+  integration_uri    = aws_lambda_function.auth.arn 
   payload_format_version = "2.0"
 }
 
@@ -158,7 +158,7 @@ resource "aws_apigatewayv2_stage" "default" {
 resource "aws_lambda_permission" "api_gw_permission" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.auth.function_name
+  function_name = aws_lambda_function.auth.function_name # Referência simples
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
 
